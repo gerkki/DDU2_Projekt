3b3efbfcad5c336da7cae5dada4499a8086357ee
gerkki <gustaf.erkki@gmail.com>
2025-01-05 20:22:27 +0100

    addUpTo - eventListener
    Skapade en eventListener med en anonym funktion som loopar igenom alla nummer och slutar när den hittat 2 nummer vars summa är lika med användarens tal.

--- addUpTo/index.js
+++ addUpTo/index.js
@@ -11,3 +11,25 @@ addUpControlsContainer.appendChild(numberInputBox);
 const findButton = document.createElement("button");
 findButton.textContent = "Find Two Cells that Add Up";
 addUpControlsContainer.appendChild(findButton);
+
+findButton.addEventListener("click", function (e) {
+
+    for (let numberBox of allNumbersBoxes) {
+        if (numberBox.classList.contains("green")) {
+            numberBox.classList.remove("green");
+        }
+    }
+
+    let inputNumber = parseInt(numberInputBox.value);
+
+    for (let numberX of allNumbersBoxes) {
+        for (let numberY of allNumbersBoxes) {
+            let sum = parseInt(numberX.textContent) + parseInt(numberY.textContent);
+            if (sum == inputNumber) {
+                numberX.classList.add("green");
+                numberY.classList.add("green");
+                return;
+            }
+        }
+    }
+});
