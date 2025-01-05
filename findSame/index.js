b18fdb0cc05a1cebe184c011dfbcf510d0426f79
gerkki <gustaf.erkki@gmail.com>
2025-01-05 15:53:38 +0100

    FindSame - Funktion och eventListener
    Skapade en funktion och en eventListener för att hitta samma nummer.

--- findSame/index.js
+++ findSame/index.js
@@ -1,9 +1,34 @@
+function findSameNumber(n) {
+
+    for (let numberBox of allNumbersBoxes) {
+        if (numberBox.classList.contains("same_number")) {
+            numberBox.classList.remove("same_number");
+        }
+    }
+
+    let amountOfCopies = 0;
+
+    for (let numberBox of allNumbersBoxes) {
+        if (numberBox.textContent == n) {
+            numberBox.classList.add("same_number");
+            amountOfCopies++;
+        }
+    }
+
+    sameTextP.textContent = `${amountOfCopies} copies of the number ${n}`;
+}
+
 const sameContainer = document.getElementById("same_controls");
 
 const sameTextP = document.createElement("p");
-sameTextP.innerHTML = "Click on a number to find copies";
+sameTextP.textContent = "Click on a number to find copies";
 sameContainer.appendChild(sameTextP);
 
 const resetButton = document.createElement("button");
 resetButton.textContent = "Reset";
 sameContainer.appendChild(resetButton);
+
+numbersDiv.addEventListener("click", function (e) {
+    const selectedNumber = e.target.textContent;
+    findSameNumber(selectedNumber);
+})
